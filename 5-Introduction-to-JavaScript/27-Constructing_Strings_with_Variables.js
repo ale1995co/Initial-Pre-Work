73c488116645faa59f2075be41308676ec5a5f7f
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:36:14 -0600

    Add code for lesson 27 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// using the concatenation operator (+), you can insert one or more variables into a string you're building.
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName= "Anthony ";
+var myStr= "My name is " + myName + " and I am well!";
