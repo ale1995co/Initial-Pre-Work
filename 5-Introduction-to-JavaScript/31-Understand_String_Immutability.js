0ef3364a65b93b26463177c9c474e3a70219ce09
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:37:23 -0600

    Add code for lesson 31 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+// immutable means that they cannot be altered once created. In JavaScript, String values are immutable.
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
