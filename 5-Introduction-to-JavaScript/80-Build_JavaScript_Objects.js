1b319da55780f618da3bdaa217b26f797e0ec028
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:27:59 -0600

    Add code for lesson 80 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Data is accesed through objects and they are called properties
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "George",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+
+};
