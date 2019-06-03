4233c0547e028661737362e78da4a1e589d152f6
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:34:54 -0600

    Add code for lesson 23 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,4 @@
+// In JavaScript you can use single or double quotes. They function the same way.
+// Make sure to use the begin the same quote and end it too.
+// especially when you have to use both quotes in a String
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
