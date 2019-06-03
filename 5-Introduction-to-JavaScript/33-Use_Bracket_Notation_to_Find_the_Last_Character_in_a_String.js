a67aafe3c1d1c94e79c17877136383a2f03033ac
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:38:05 -0600

    Add code for lesson 33 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,10 @@
+// To find the last character in the string by bracket notation is to substract one from the string's length.
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length -1];
