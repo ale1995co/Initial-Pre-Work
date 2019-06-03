344aff909da4a254cb0da38cacb18ea386120476
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:36:52 -0600

    Add code for lesson 29 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,14 @@
+// Should be to find the length of a string value by writing .length after the strong variable or string literal
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
