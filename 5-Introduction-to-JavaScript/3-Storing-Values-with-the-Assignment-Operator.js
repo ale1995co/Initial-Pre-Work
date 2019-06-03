491d02040a9d58c2ca2925976e39be60786d595c
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:26:56 -0600

    Add code for lesson 3 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+// TO store a value in a variable to use an assignment operator.
+// The assignment goes from righ to left. Everything to the right of the = operator is resolved before the value is assigned to the variab le to the left of the operator
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a=7;
+var b=a;
