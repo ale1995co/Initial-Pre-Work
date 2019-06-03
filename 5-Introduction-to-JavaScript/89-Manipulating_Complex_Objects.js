cb2c624352884a166f1bb892ab82911cbcd25ba4
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:30:22 -0600

    Add code for lesson 89 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+// Make sure to add commas,A JavaScript object is one way to handle flexible data. They allow for arbitrary combinations of strings, numbers, booleans, arrays, functions, and objects.
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+   {
+    "artist": "Michael Jackson",
+    "title": "Beat It",
+    "release_year": 1985,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+  }
+];
