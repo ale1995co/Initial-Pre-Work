9426a221d29de6e3ad800566c6ce8f9d568c9e6f
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:28:28 -0600

    Add code for lesson 82 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,12 @@
+// bracket notation ([]). If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
