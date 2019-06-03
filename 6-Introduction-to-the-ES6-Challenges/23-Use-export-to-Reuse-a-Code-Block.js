81f5a998dd1312e65bd8cd8eeae10a224a736e1c
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:47:18 -0600

    Add code for lesson 23 of ES6 module and comment what I learned.

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,4 @@
+// When we want some code - a function, or a variable - to be usable in another file, we must export it in order to import it into another file.
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
