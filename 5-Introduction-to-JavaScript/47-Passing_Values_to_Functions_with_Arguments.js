6d6f83a24d1d0a83818f5d1bc7883a92b96ad0a5
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:11:54 -0600

    Add code for lesson 47 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,14 @@
+// Parameterd are variables that act like placeholders for values to be input to a function when it is called.
+// The actual values that are input into a function they are known as arguments.
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b){
+  console.log(a + b);
+}
+functionWithArgs(7, 9);
+functionWithArgs(1, 2);
