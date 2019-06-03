8b4aba5fc76550770951038e84509fa8bee4331e
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:38:55 -0600

    Add code for lesson 36 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,6 @@
+// array varibales can be use to store several pieces of data in one place.
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Anthony", 23];
