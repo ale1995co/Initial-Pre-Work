a2d29145d9de598a4e4154488c3be6b5462430f1
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:35:13 -0600

    Add code for lesson 1 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,11 @@
+// Two reasos to use escaping characters.irst is to allow you to use characters you might not otherwise be able to type out, such as a backspace.
+//second is to allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean.
+// \'	single quote
+// \"	double quote
+// \\	backslash
+// \n	newline
+// \r	carriage return
+// \t	tab
+// \b	backspace
+// \f	form feed
+var myStr="FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
