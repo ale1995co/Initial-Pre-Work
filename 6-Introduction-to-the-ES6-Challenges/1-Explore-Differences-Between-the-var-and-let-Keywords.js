0ee71c0c0988eab5d48ac99291736ca183a650fb
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:39:18 -0600

    Add code for lesson 1 of ES6 module and comment what I learned.

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+//  when using let, a variable with the same name can only be declared once
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
