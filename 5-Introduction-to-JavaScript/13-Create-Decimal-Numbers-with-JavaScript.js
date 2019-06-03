035565afcb6520c11cb993a0163c54f281cc7573
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:30:55 -0600

    Add code for lesson 13 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,5 @@
+// decimal numbers can be in variables also. They are referred to as floating point numbers or floats
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal= 5.7;
