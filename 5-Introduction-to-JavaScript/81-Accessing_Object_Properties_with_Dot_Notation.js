b8ab3929646bebdff68d89c68b62c5271bd0bb25
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:28:13 -0600

    Add code for lesson 81 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,12 @@
+// To access the property of an object is dot noatation(.) and bracket notation([])
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat     // Change this line
+var shirtValue = testObj.shirt    // Change this line
