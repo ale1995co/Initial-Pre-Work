865e7abc6dc3abbc734da1d844ff26a55fdea657
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:26:13 -0600

    Add code for lesson 2 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+// In JavaScript provides 7 data types. undefined, null, boolean, string, symbol, number, and object.
+// Computer varibales differ from mathematical variables that they can store different valuesd at different times
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
