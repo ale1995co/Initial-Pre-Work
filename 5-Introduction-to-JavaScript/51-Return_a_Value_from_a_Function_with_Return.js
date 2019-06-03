28dec345552b66a5c9757f906a0211895be57c7b
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:13:13 -0600

    Add code for lesson 51 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+//We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(minusSeven(10));
+console.log(timesFive(5));
+console.log(timesFive(2));
+console.log(timesFive(0));
