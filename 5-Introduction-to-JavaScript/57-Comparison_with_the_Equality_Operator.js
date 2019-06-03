c972a618f34e2b69a2e20a0cb3934fc2382897b6
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:15:03 -0600

    Add code for lesson 57 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,11 @@
+//The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're equivalent or false if they are not.
+// Setup
+function testEqual(val) {
+  if (val ==12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
