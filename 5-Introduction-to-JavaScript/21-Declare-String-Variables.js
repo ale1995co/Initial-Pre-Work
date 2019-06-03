4a2986a3b33495a07c6a54454111e542896793b9
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:34:02 -0600

    Add code for lesson 21 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+// a string is a series of zero or more characters that is enclosed in single or double quotes
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Anthony";
+var myLastName = "Le";
