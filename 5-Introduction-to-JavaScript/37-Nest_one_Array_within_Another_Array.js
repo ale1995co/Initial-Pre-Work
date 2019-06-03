efd9bac9321bf87b45b6bcb15e8857dfb82537f6
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:39:24 -0600

    Add code for lesson 37 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+// Multi-dimensional Array is a nested array
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Jokic", 15], ["Nuggets", 2019]];
