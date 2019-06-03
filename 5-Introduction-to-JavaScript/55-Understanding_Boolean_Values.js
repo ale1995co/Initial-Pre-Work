32cfb71799e4a9f317e0bae3161b315fd4fbf173
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:15:31 -0600

    Add code for lesson 55 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// a booleans may be only be one of two values: true or false.
+// They are like on-off switches
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
