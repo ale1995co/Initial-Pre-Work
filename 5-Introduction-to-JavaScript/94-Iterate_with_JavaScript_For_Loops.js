99fa71788544d5612cecd95665e20349ae4dfa13
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:31:50 -0600

    Add code for lesson 94 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+// For loops are declared with three optional expressions separated by semicolons:
+//for ([initialization]; [condition]; [final-expression])
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i =1; i < 6; i++) {
+    myArray.push(i);
+}
