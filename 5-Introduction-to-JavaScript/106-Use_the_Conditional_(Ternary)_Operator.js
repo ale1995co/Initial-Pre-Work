d8b5573601e5cef8a3e25bd348cf799dd2907855
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:34:35 -0600

    Add code for lesson 106 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,11 @@
+// The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+// The syntax is:
+
+// condition ? statement-if-true : statement-if-false;
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
+checkEqual(1, 1);
+checkEqual(1, -1);
