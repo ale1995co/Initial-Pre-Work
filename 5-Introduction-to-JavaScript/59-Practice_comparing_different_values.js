c7aa8baf83269f9f69ce3df8e03c2c41328cd1ef
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:16:06 -0600

    Add code for lesson 59 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,14 @@
+// In JavaScript, you can determine the type of a variable or a value with the typeof operator, as follows:
+
+// typeof 3 // returns 'number'
+// typeof '3' // returns 'string'
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
