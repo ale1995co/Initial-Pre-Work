b91bd3a100e04577393ae067c2319ad8b8b4892b
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:34:09 -0600

    Add code for lesson 104 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,8 @@
+//The parseInt() function parses a string and returns an integer.
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
+convertToInteger("77");
+convertToInteger("JamesBond");
