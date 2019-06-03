9b63df3225b8ee6529ce4105dcb12127516bb763
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:40:03 -0600

    Add code for lesson 4 of ES6 module and comment what I learned.

---
+++ 6-Introduction-to-the-ES6-Challenges/4-Mutate-an-Array-Declared-with-const.js
@@ -0,0 +1,12 @@
+//  it is important to understand that objects (including arrays and functions) assigned to a variable using const are still mutable. Using the const declaration only prevents reassignment of the variable identifier.
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+ // s= [2, 5, 7] <- this is invalid
+  // change code above this line
+}
+editInPlace();
