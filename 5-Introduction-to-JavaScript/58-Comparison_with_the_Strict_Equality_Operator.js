bd6bcc248b3a1be09ced59836ea09453bad0435a
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:15:53 -0600

    Add code for lesson 58 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,13 @@
+//Strict equality (===) is the counterpart to the equality operator (==).
+//However, unlike the equality operator, which attempts to convert both values being compared to a common type, the strict equality operator does not perform a type conversion
+// If the values being compared have different types, they are considered unequal, and the strict equality operator will return false.
+// Setup
+function testStrict(val) {
+  if (val ===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(7);
