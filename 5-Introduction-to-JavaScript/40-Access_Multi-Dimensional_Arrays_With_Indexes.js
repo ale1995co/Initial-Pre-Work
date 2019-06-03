d6f7725c057c81f8d3f6c2443f7eaae085997331
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:09:43 -0600

    Add code for lesson 40 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,6 @@
+// // multi-dimensional array is an array of arrays. The first set of brackets to aceess the array is the outer-most array, and each additional pair of brackets refer to teh next level.
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
