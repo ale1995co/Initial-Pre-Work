e7406511fcb0c649d5209e2e144bf2eadedc1fd6
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:47:32 -0600

    Add code for lesson 25 of ES6 module and comment what I learned.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,6 @@
+// uppose you have a file that you wish to import all of its contents into the current file. This can be done with the import * syntax.
+"use strict";
+import * as str from "capitalize_strings";
+// this syntax if only one value is being exported from a file. It is also used to create a fallback value for a file or module.
+"use strict";
+ export default function subtract(x,y) {return x - y;}
