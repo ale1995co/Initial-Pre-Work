68b7d34ae1035752ca07f5008640ed9f68d2c227
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:35:40 -0600

    Add code for lesson 25 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,7 @@
+// when a + operator is used with a string value, it is called concatenation operator.
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr= "This is the start. " + "This is the end.";
