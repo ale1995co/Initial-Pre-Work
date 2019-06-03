2c6020ec9cf798cbbfb034feb0120aaa9bf42620
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:20:00 -0600

    Add code for lesson 68 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,17 @@
+// With an else statement, an alternate block of code can be executed.
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    return "Bigger than 5";
+  } else {
+    return "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
