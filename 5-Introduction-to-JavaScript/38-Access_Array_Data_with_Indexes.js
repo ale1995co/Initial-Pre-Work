a72257f0c5c0005c614050194e1ab5d8583b4322
Anthony Le <ale1995co@gmail.com>
2019-06-03 02:09:08 -0600

    Add code for lesson 1 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// arrays use zero-based indexing like strings. To find the data inside arrays using indexes by using the same bracket notation that strings use.
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
