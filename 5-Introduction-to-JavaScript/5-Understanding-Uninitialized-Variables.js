663c736edac546ded2754d4828b76fedb88f0185
Anthony Le <ale1995co@gmail.com>
2019-06-03 01:27:41 -0600

    Add code for lesson 5 of JavaScript  module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// JavaScript are declared, the intial value is undefined. The mathematical operation , the undefined varibale result be NaN( not a number)
+// When concatenate a strong the undefined variable, the literal string would be "undefined"
+// Initialize these three variables
+var a=5;
+var b=10;
+var c="I am a"
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
